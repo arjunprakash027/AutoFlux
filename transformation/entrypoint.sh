89eaa8cd5c297d6fc9072d5b83a3fbf2e20c23be
Arjun <arjunprakash027@gmail.com>
2025-01-31 08:42:22 +0530

    Initializing the repo

---
+++ transformation/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run the dbt processes in entry
+echo "Running deps"
+dbt deps
+
+echo "Running Seed"
+dbt seed
+
+echo "Data Loaded"
+dbt run
+
+echo "Transformation Done!"
+
+# Keep the container hanging
+exec tail -f /dev/null
+
