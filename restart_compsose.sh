3c517aeb4102aaefcb4d24526c4bc74a74ced982
Arjun <arjunprakash027@gmail.com>
2025-02-09 04:40:17 +0530

    spark working in ml container

--- restart_compsose.sh
+++ restart_compsose.sh
@@ -1,5 +1,3 @@
 docker-compose down
-
-docker-compose build spark spark_worker metastore-db transformation
-
-docker-compose up -d transformation
+docker-compose build spark spark_worker metastore-db transformation ml
+docker-compose up -d ml
--- transformation/entrypoint.sh
+++ transformation/entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Run the dbt processes in entry
-# echo "Running deps"
+
+echo "Running deps"
 dbt deps
 
+until nc -zv -w30 spark-server 10000
+do
+    echo "Waiting for spark server to start..."
+    sleep 5
+done
+# Seeding data
 echo "Running Seed"
-#dbt seed
+dbt seed
 
 echo "Data Loaded"
 # dbt run
