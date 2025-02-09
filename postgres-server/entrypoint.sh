7a908e3af180dc72798be18883bdfc21a4dc9b36
Arjun <arjunprakash027@gmail.com>
2025-02-09 18:08:52 +0530

    ready to be merged to main

--- postgres-server/entrypoint.sh
+++ postgres-server/entrypoint.sh
@@ -7,6 +7,7 @@ echo "Starting postgres server..."
 docker-entrypoint.sh postgres &
 
 echo "until postgres is ready"
+
 until pg_isready -h localhost -p 5432
 do
     echo "waiting for pg to start"
@@ -14,15 +15,6 @@ do
 done
 
 echo "pg started"
-
-# if psql -U user -d metastore -tAc "SELECT 1 FROM information_schema.tables WHERE table_name='DBS';" | grep -q 1; then
-#     echo "Metastore already initialized"
-
-# else
-#     echo "Initializing metastore..."
-#     psql -U user -d metastore -f /hive-schema.sql
-#     echo "Metastore initialized"
-# fi
 
 hive --service metastore --hiveconf hive.metastore.uris=thrift://0.0.0.0:9083
 
