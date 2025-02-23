8059bb1f03f723c5f98dffcf2c2f8ef7ae1cab7a
Arjun <arjunprakash027@gmail.com>
2025-02-23 11:03:30 +0530

    changes to restart bash script

--- restart_compsose.sh
+++ restart_compsose.sh
@@ -1,3 +1,12 @@
 docker-compose down
-docker-compose build spark spark_worker metastore-db transformation ml
+
+if [ "$1" == "no-transformation" ]; then
+    echo "Building without transformation"
+    docker-compose build spark spark_worker metastore-db ml
+else
+    echo "Building with transformation"
+    docker-compose build spark spark_worker metastore-db transformation ml
+    docker-compose up -d transformation
+fi
+
 docker-compose up -d ml
